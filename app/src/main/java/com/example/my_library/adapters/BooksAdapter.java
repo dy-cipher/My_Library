7d8b41394ef1978c504e7122851229fea84d5432
dy-cipher <wendygastien16@gmail.com>
2022-10-07 14:53:38 -0700

    booksAdapter created

--- app/src/main/java/com/example/my_library/adapters/BooksAdapter.java
+++ app/src/main/java/com/example/my_library/adapters/BooksAdapter.java
@@ -1,4 +1,45 @@
 package com.example.my_library.adapters;
 
-public class BooksAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.my_library.R;
+
+public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.ViewHolder> {
+
+    private static final String TAG = "BooksAdapter";
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView ivBookImage;
+        private TextView tvBookName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivBookImage = (ImageView) itemView.findViewById(R.id.ivBookImage);
+            tvBookName = (TextView) itemView.findViewById(R.id.tvBookName);
+        }
+    }
 }
