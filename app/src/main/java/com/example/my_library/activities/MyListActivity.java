acb543a4cd242e7c2151a2044591753bab249aae
dy-cipher <wendygastien16@gmail.com>
2022-10-11 08:41:37 -0700

    Mylist page added

---
+++ app/src/main/java/com/example/my_library/activities/MyListActivity.java
@@ -0,0 +1,16 @@
+package com.example.my_library.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.my_library.R;
+
+public class MyListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_list);
+    }
+}
